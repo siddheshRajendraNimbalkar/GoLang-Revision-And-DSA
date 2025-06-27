6e430af93e9e811ea981f33933831383cfb639fa
Nimbalkar Siddhesh <nimbalkarsiddhesh29@gmail.com>
2025-06-27 23:31:46 +0530

    Swaping interface

--- Revision/Beginner/SwapVar/Swap.go
+++ Revision/Beginner/SwapVar/Swap.go
@@ -1,1 +1,18 @@
 package swapvar
+
+import (
+	"fmt"
+	"log"
+	"reflect"
+)
+
+func SwapVar(a, b *interface{}) {
+	if reflect.TypeOf(*a) != reflect.TypeOf(*b) {
+		log.Panicln("enter same data type")
+	} else {
+		fmt.Println("a: ", *a, "b: ", *b)
+		*a, *b = *b, *a
+		fmt.Println("a: ", *a, "b: ", *b)
+	}
+
+}
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import helloworld "github.com/siddheshRajendraNimbalkar/GoLang-Revision-And-DSA/Revision/Beginner/HelloWorld"
+import (
+	"fmt"
+
+	helloworld "github.com/siddheshRajendraNimbalkar/GoLang-Revision-And-DSA/Revision/Beginner/HelloWorld"
+	swapvar "github.com/siddheshRajendraNimbalkar/GoLang-Revision-And-DSA/Revision/Beginner/SwapVar"
+)
+
+func Beginner() {
+	helloworld.HelloWorld()
+	var x interface{} = 100
+	var y interface{} = 500
+
+	swapvar.SwapVar(&x, &y)
+
+	fmt.Println("x: ", x, "y: ", y)
+}
 
 func main() {
-	helloworld.HelloWorld()
+	Beginner()
 }
